ec2957ac11069617c23e03bcc8936e038b27f702
th3y3m <truongtanhuy3006@gmail.com>
2024-11-18 15:42:33 +0700

    init

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using MLDemo;
+
+namespace MLDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Add input data
+            var sampleData = new SentimentModel.ModelInput()
+            {
+                Col0 = "Then, as if I hadn't wasted enough of my life there, they poured salt in the wound by drawing out the time it took to bring the check."
+            };
+
+            // Load model and predict output of sample data
+            var result = SentimentModel.Predict(sampleData);
+
+            // If Prediction is 1, sentiment is "Positive"; otherwise, sentiment is "Negative"
+            var sentiment = result.PredictedLabel == 1 ? "Positive" : "Negative";
+            Console.WriteLine($"Text: {sampleData.Col0}\nSentiment: {sentiment}");
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+
+        }
+    }
+}
